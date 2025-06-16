1539d3b52bac8f3a5dbfae89f8893f308fd0d08c
Rojin4455 <rojinsaji35@gmail.com>
2025-06-16 15:55:50 +0530

    refresh all token logic in tasks.py file

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -6,31 +6,33 @@ from decouple import config
 
 @shared_task
 def make_api_call():
-    credentials = GHLAuthCredentials.objects.first()
-    
-    print("credentials tokenL", credentials)
-    refresh_token = credentials.refresh_token
+    tokens = GHLAuthCredentials.objects.all()
 
+    for credentials in tokens:
     
-    response = requests.post('https://services.leadconnectorhq.com/oauth/token', data={
-        'grant_type': 'refresh_token',
-        'client_id': config("GHL_CLIENT_ID"),
-        'client_secret': config("GHL_CLIENT_SECRET"),
-        'refresh_token': refresh_token
-    })
-    
-    new_tokens = response.json()
+        print("credentials tokenL", credentials)
+        refresh_token = credentials.refresh_token
 
-    obj, created = GHLAuthCredentials.objects.update_or_create(
-            location_id= new_tokens.get("locationId"),
-            defaults={
-                "access_token": new_tokens.get("access_token"),
-                "refresh_token": new_tokens.get("refresh_token"),
-                "expires_in": new_tokens.get("expires_in"),
-                "scope": new_tokens.get("scope"),
-                "user_type": new_tokens.get("userType"),
-                "company_id": new_tokens.get("companyId"),
-                "user_id":new_tokens.get("userId"),
+        
+        response = requests.post('https://services.leadconnectorhq.com/oauth/token', data={
+            'grant_type': 'refresh_token',
+            'client_id': config("GHL_CLIENT_ID"),
+            'client_secret': config("GHL_CLIENT_SECRET"),
+            'refresh_token': refresh_token
+        })
+        
+        new_tokens = response.json()
 
-            }
-        )
+        obj, created = GHLAuthCredentials.objects.update_or_create(
+                location_id= new_tokens.get("locationId"),
+                defaults={
+                    "access_token": new_tokens.get("access_token"),
+                    "refresh_token": new_tokens.get("refresh_token"),
+                    "expires_in": new_tokens.get("expires_in"),
+                    "scope": new_tokens.get("scope"),
+                    "user_type": new_tokens.get("userType"),
+                    "company_id": new_tokens.get("companyId"),
+                    "user_id":new_tokens.get("userId"),
+
+                }
+            )
